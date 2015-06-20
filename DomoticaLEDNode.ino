f901afc98334728c2644aaad0263051a12e6f01c
Gert Vermeersch <vermeersch.gert@gmail.com>
2015-06-20 22:28:49 +0200

    Redesign to use Wifi chip
    I decided to use a ESP8266 (the popular low cost wifi chip) for this
    project. As TCP/IP is a better way to communicate with the unit. The
    NRF905 chips work fine but I had problems with the communication between
    the RPi and Arduino sending the radio messages

--- DomoticaLEDNode.ino
+++ DomoticaLEDNode.ino
@@ -1,64 +1,134 @@
-#include <NRF905.h>
-#include <domotica.h>
-#include <SPI.h>
+#include <SoftwareSerial.h>
+#include "ESP8266.h"
 
-Domotica controller;
-char msg[32];
-const int switchport = A0;
-boolean on = false;
-char* result;
-char packet[MSG_LEN];
+#define SSID        "Dovy Keukens"
+#define PASSWORD    "S3cur1ty"
+#define ESP8266_USE_SOFTWARE_SERIAL
+#define RED          10
+#define BLUE         5
+#define GREEN        6
+
+SoftwareSerial wifiSerial(3,2);
+ESP8266 wifi(wifiSerial);
+
 
 void setup() {
-  Serial.begin(9600);
-  controller = Domotica();
-  controller.setDebug(true);
-  controller.init(2);  
+  pinMode(RED, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  //start the normal serial port 
+  Serial.begin(115200);
+  Serial.println("Arduino started");
+  Serial.println("self test");
+  testLED();
+  Serial.println("Starting ESP8266...");
   
-  msg[0] = 1;
-  delay(200);
-  pinMode(switchport, OUTPUT);
-  pinMode(13,OUTPUT);
-  Serial.println("Starting I/O self-test");
-  delay(100);
-  digitalWrite(switchport, HIGH);
-  delay(500);
-  digitalWrite(switchport, LOW);
-  delay(100);
-  packet[0] = 'a';
-  packet[1] = 'b';
-  packet[2] = '\0';
+  Serial.print("FW Version: ");
+  Serial.println(wifi.getVersion().c_str());
+  while(!startWifiServer());
 }
 
 void loop() {
+  String strBuffer;
+  uint8_t buffer[128] = {0};
+    uint8_t mux_id;
+    uint32_t len = wifi.recv(&mux_id, buffer, sizeof(buffer), 100);
+    if (len > 0) {
+        
+        strBuffer = String((char*)buffer);
+        Serial.print("]\r\n");
+        
+        parseBuffer(mux_id, strBuffer);
+       
+    }
+
+}
+
+void testLED() {
+  analogWrite(RED, 0);
+  analogWrite(BLUE, 0);
+  analogWrite(GREEN, 0);
+  for(int i = 0; i<255; i++) {
+    analogWrite(RED, i);
+    delay(50);
+  }  
+  analogWrite(RED, 0);
+  for(int i = 0; i<255; i++) {
+    analogWrite(BLUE, i);
+    delay(50);
+  } 
+  analogWrite(BLUE, 0);
+  for(int i = 0; i<255; i++) {
+    analogWrite(GREEN, i);
+    delay(50);
+  } 
+  analogWrite(GREEN, 0);
+  for(int i = 0; i<255; i++) {
+    analogWrite(RED, i);
+    analogWrite(GREEN, i);
+    analogWrite(BLUE, i);
+    delay(50);
+  } 
+  analogWrite(RED, 0);
+  analogWrite(BLUE, 0);
+  analogWrite(GREEN, 0);
+}
+
+bool parseBuffer(uint8_t mux_id, String strBuffer) {
+  String msg;
   
-  Serial.println("waiting for msg");
-  while(!controller.checkNewMsg()) {
+  Serial.println("Received: ");
+  Serial.println(strBuffer);
+  if(strBuffer.indexOf("hello") > -1) {
+     sendString(mux_id, String("Hi there, I'm a LED controller\r"));
+  }  
+  if(strBuffer.indexOf("exit") > -1) {
+     sendString(mux_id, String("Bye!"));    
+     wifi.releaseTCP(mux_id); 
+  }  
+  if(strBuffer.indexOf("Set") > -1) {
     
   }
-  Serial.println("msg received");
-  result = controller.getMsg();
-  for(int i = 0; i < BUF_LEN; i++) 
-    Serial.print(result[i]);
-  Serial.print("\n\r");
-  for(int i = 0; i < BUF_LEN; i++) 
-    Serial.print(result[i],HEX);
-  Serial.print("\n\r"); 
-  command();
-  controller.sendToNode(0,packet);
-    
-  
-  //controller.sendToNode(1,msg);
   
 }
 
-void command() {
-  Serial.print(result[4], DEC);
-  Serial.print(result[8], DEC);
-  if(result[4] == 1 && result[8] == 1) {
-    on = true;
-  } else if(result[4] == 0 && result[8] == 1) {
-    on = false;
+void sendString(uint8_t mux_id, String msg) {
+    char txtbuffer[128];
+    msg.toCharArray(txtbuffer, msg.length());
+    wifi.send(mux_id, (uint8_t*)txtbuffer, (uint32_t)msg.length()+1);
+}
+
+bool startWifiServer() {
+  bool result = true;
+  if (wifi.setOprToStation()) {
+        Serial.print("Chip was set to station mode\r\n");
+        Serial.println("Connecting to hard coded Wifi");
+        if (wifi.joinAP(SSID, PASSWORD)) {
+              Serial.print("Join AP - success\r\n");
+              Serial.print("IP: ");       
+              Serial.println(wifi.getLocalIP().c_str());
+              if (wifi.enableMUX()) {
+                Serial.print("multiple connections ok\r\n");
+                if (wifi.startTCPServer(23)) {
+                  Serial.print("start telnet tcp server ok\r\n");
+                } else {
+                  Serial.print("start telnet tcp server error\r\n");
+                  result = false;
+                }
+              } else {
+                Serial.print("multiple err\r\n");
+                result = false;
+              } 
+        } else {
+           Serial.println("failed to join AP"); 
+           result = false;
+        }  
+  } else {
+        Serial.print("Chip was not set to station mode - malfunction\r\n");
+        result = false;
   }
-  digitalWrite(switchport, on);
+    
+  
+  return result;
+  
 }
